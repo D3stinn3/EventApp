573f8a0647a898bf87bcdd4b819d5e76a5314cf8
D3stinn3 <paynedestinne@gmail.com>
2023-04-25 19:34:43 +0300

    Views

--- EventApplication/base/views.py
+++ EventApplication/base/views.py
@@ -3,5 +3,7 @@ from .models import User, Event, Submission
 
 # Create your views here.
 def home_page(request):
-    users = User.object.filter(party=True)
+    users = User.objects.filter(party=True)
+    context = {'users': users, 'events': events}
+    events = Event.objects.all()
     return render(request, "home.html")
