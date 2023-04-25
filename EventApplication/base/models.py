9f14e30b6dcba71fa292bf1f0da7ea91c8e27b85
D3stinn3 <paynedestinne@gmail.com>
2023-04-25 10:15:19 +0300

    Models Conf

--- EventApplication/base/models.py
+++ EventApplication/base/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
