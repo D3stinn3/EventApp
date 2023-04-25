b9cf2c1da95b56c756b25d8ffcdd4170efbcea71
D3stinn3 <paynedestinne@gmail.com>
2023-04-25 10:44:51 +0300

    Modelling

--- EventApplication/base/models.py
+++ EventApplication/base/models.py
@@ -9,3 +9,26 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+class Event(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, blank=True)
+    date = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Submission(models.Model):
+    participant = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
+    # models.CASCADE deletes the model tree ie parent, child
+    # models.SET_NULL saves an instance of a submission
+    details = models.TextField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return str(self.event) + '---' + str(self.participant)
+    
+    
+    
