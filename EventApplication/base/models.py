16c834cb9f96209ff381d22319844bd4d8e63c45
D3stinn3 <paynedestinne@gmail.com>
2023-04-25 11:29:38 +0300

    Slight Model Change

--- EventApplication/base/models.py
+++ EventApplication/base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    # AbstractUser class implements a fully featured User model with admin-compliant permissions
     name = models.CharField(max_length=100, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
