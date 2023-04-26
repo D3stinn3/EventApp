79a37d6ec384f887141893ba81eb696188a38a4a
D3stinn3 <paynedestinne@gmail.com>
2023-04-26 12:10:18 +0300

    Pk

--- EventApplication/base/urls.py
+++ EventApplication/base/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('event/<str:pk>/', views.event_page, name="event"),
     path('eventconfirm/<str:pk>/', views.registration_confirmation, name="confirm"),
     path('user/<str:pk>/', views.user_page, name="userprofile"),
-    path('account/', views.account_page, name="account")
+    path('account/<str:pk>/', views.account_page, name="account")
 ]
